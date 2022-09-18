79cd5975674b1930990c3a8d895606a41b4939b7
callumrowston <callum.rowsto@gmail.com>
2022-09-18 14:52:38 +1000

    Began very basic input guess logic

--- wordle_game.py
+++ wordle_game.py
@@ -1,18 +1,38 @@
-import wordle_logic
+from wordle_logic import WordleLogic
 
 # Interface between user and game. 
 
 # TODO: Welcome user to game, options for quit, rules, stats, game options
-
+def game_start():
+    print("Welcome to PyWordle...")
+    wordle = WordleLogic('CRATE')
+    while not wordle.user_wins:
+        user_guess()
+        
 # TODO: Ask user for guess
+def user_guess():
+    guess = input("Enter a 5 letter word.\n")
+    return guess
 
 # TODO: Dispaly user guess as colored result 
+def display_guess():
+    pass
 
 # TODO: Display end game win/loss. Play again? Quit? 
+def end_menu():
+    pass
 
 # TODO: Display stats
+def display_stats():
+    pass
 
 # TODO: Format game interface to look user friendly
 # Spacing
-# _ _ _ _ _ to signify remaining attempts
+# _ _ _ _ _ to signify a remaining attempt
 # Border?
+
+# Gameplay Loop
+
+if __name__ == '__main__':
+    game_start()
+    
--- wordle_logic.py
+++ wordle_logic.py
@@ -1,13 +1,30 @@
-import wordle_game
 import random
 
 # All logic of game. Sends computed logic to wordle.game. 
+class WordleLogic:
 
+    def __init__(self, secret_word):
+        self.secret_word = secret_word
+        self.guesses = []
+        self.attempts = 6
+
+    def user_wins(self):
+        return self.secret_word in self.guesses
+            
+    def add_user_guess(self, user_guess):
 # TODO: Randomly choose secret word from word_list
+    # def pick_secret_word(self):
+    #     # secret_word = random.choice(word_list) 
+    #     ## hardcode word for testing while developing
+    #     secret_word = 'crate'.upper()
+    #     return secret_word
+
 
 # TODO: Receive user guess from wordle_game
 # Check guess is 5 letters, is in word_list
 # Raise exception(s) if not 5 letters, not in word_list
+# Add user guess to guess list
+
 
 # TODO: Compare user guess against secret word
 # Wrong letters = grey
@@ -15,15 +32,20 @@ import random
 # Correct letters, correct spot = green
 # Return colored string to wordle_game
 
+
 # TODO: Track and loop attempts
 # Increment attempts counter each time user input received
 # Prompt user for guess while attempts < 6 and user guess != secret word
 # If attempts = 6, check if user guess = secret word, else end game
 
+
 # TODO: Game end scenarios
 # If user guess = secret word, tell wordle_game user wins
 # If user guess != secret word and attempts = 6, tell wordle_game user loses
 # User quits
+    # def user_wins(self):
+    #     if self.secret_word in self.guesses:
+    #         return True
 
 # TODO: Game stats and highscore
 # Store game results when game ends 
