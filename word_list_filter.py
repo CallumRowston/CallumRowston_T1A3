bb54131bcbc6062d4cc060bcc91a9c4a385e15e3
callumrowston <callum.rowsto@gmail.com>
2022-09-22 16:43:09 +1000

    Refactor

--- word_list_filter.py
+++
@@ -1,9 +0,0 @@
-word_word = []
-
-with open('word_list.txt', 'r') as f:
-    for word in f.readlines():
-        word_word.append(word.strip())
-
-with open('word_list_6.txt', 'w') as g:
-    for word in word_word:
-        g.write(word.upper() + "\n")
